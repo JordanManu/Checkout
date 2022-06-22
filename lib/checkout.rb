8d3d3d93926acc1f1ac2581686ef41aa53c4ff61
Jordan <jordanmanu@live.com.au>
2022-06-22 12:12:44 +0100

    test passing with amount of items

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,9 @@
 class Checkout 
  
   ITEMS = {
-    "Lavender heart" => 9.25
+    "Lavender heart" => 9.25, 
+    "Personalised cufflinks" => 45.00,
+    "Kids T-shirt" => 19.95
   }
 
   def initialize(promotional_rules)
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,16 +1,19 @@
 require 'checkout'
 
 describe Checkout do 
-  context "items" do 
+  context "Items" do 
     let (:june_promotion) { 10 }
     let (:co) { Checkout.new(june_promotion) }
 
     it 'is initialized with an item' do
-      expect(Checkout::ITEMS).to eq "Lavender heart" => 9.25
+      expect(Checkout::ITEMS.first).to eq ["Lavender heart", 9.25]
     end
 
     it 'has prices for each item' do 
       expect(Checkout::ITEMS["Lavender heart"]).to eq 9.25
+    end
+    it 'has 3 items' do 
+      expect(Checkout::ITEMS.count).to eq 3
     end
   end
 end
