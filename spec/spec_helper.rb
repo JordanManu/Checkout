9ef598398dd70ba5a91848513ccfa8b87724d062
Jordan <jordanmanu@live.com.au>
2022-06-22 18:23:20 +0100

    removed simplecov console

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-                                                                 SimpleCov::Formatter::Console
-                                                               ])
 SimpleCov.start
 
 RSpec.configure do |config|
