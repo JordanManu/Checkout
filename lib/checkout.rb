fc457f072d842997b5fea7ce5e29d28c24e877cf
Jordan <jordanmanu@live.com.au>
2022-06-22 12:30:21 +0100

    Added remove item method and passing test

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,18 +1,25 @@
-class Checkout 
+# frozen_string_literal: true
 
+class Checkout
   attr_reader :basket
- 
-  ITEMS = {
-    "Lavender heart" => 9.25, 
-    "Personalised cufflinks" => 45.00,
-    "Kids T-shirt" => 19.95
-  }
 
-  def initialize(promotional_rules)
+  ITEMS = {
+    'Lavender heart' => 9.25,
+    'Personalised cufflinks' => 45.00,
+    'Kids T-shirt' => 19.95
+  }.freeze
+
+  def initialize(_promotional_rules)
     @basket = []
   end
-  
+
   def scan(item)
+    raise 'That item is not available' unless ITEMS.key?(item)
+
     @basket.push(item)
+  end
+
+  def remove_item(item)
+    @basket.delete(item)
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,29 +1,44 @@
+# frozen_string_literal: true
+
 require 'checkout'
 
-describe Checkout do 
-  context "Items" do 
-    let (:june_promotion) { 10 }
-    let (:co) { Checkout.new(june_promotion) }
+describe Checkout do
+  
+  context 'Items' do
+    let(:june_promotion) { 10 }
+    let(:co) { Checkout.new(june_promotion) }
 
     it 'is initialized with an item' do
-      expect(Checkout::ITEMS.first).to eq ["Lavender heart", 9.25]
+      expect(Checkout::ITEMS.first).to eq ['Lavender heart', 9.25]
     end
 
-    it 'has prices for each item' do 
-      expect(Checkout::ITEMS["Lavender heart"]).to eq 9.25
+    it 'has prices for each item' do
+      expect(Checkout::ITEMS['Lavender heart']).to eq 9.25
     end
-    it 'has 3 items' do 
+    it 'has 3 items' do
       expect(Checkout::ITEMS.count).to eq 3
     end
   end
-  context "Scan" do 
-    let (:june_promotion) { 10 }
-    let (:co) { Checkout.new(june_promotion) }
 
-    it 'adds multiple items to the basket' do 
-      co.scan("Lavender heart")
-      co.scan("Personalised cufflinks")
-      expect(co.basket).to eq ["Lavender heart", "Personalised cufflinks"]
+  context 'Scan items' do
+    let(:june_promotion) { 10 }
+    let(:co) { Checkout.new(june_promotion) }
+
+    it 'adds multiple items to the basket' do
+      co.scan('Lavender heart')
+      co.scan('Personalised cufflinks')
+      expect(co.basket).to eq ['Lavender heart', 'Personalised cufflinks']
+    end
+
+    it 'raises an error if user selects an item that is not available' do
+      expect { co.scan('Mens T-shirt') }.to raise_error 'That item is not available'
+    end
+
+    it 'allows user to remove an item from the basket' do 
+      co.scan('Lavender heart')
+      co.scan('Personalised cufflinks')
+      co.remove_item('Personalised cufflinks')
+      expect(co.basket).not_to include 'Personalised cufflinks'
     end
   end
 
